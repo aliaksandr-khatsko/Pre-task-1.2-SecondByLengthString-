88f6be3d4145e41fb873853e540f7d8e7018a348
MINSK\Aliaksandr_Khatsko <aliaksandr.kh@gmail.com>
2016-11-18 19:21:06 +0300

    First commit pre-task 1.2 SecondByLengthString

---
+++ SecondByLengthString/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondByLengthString
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+
+        {
+
+            message("first");
+            string firstLine = Console.ReadLine();
+            message("second");
+            string secondLine = Console.ReadLine();
+            message("third");
+            string thirdLine = Console.ReadLine();
+            message("forth");
+            string forthLine = Console.ReadLine();
+            message("fifth");
+            string fifthLine = Console.ReadLine();
+            string[] stringArray = { firstLine, secondLine, thirdLine, forthLine, fifthLine };
+
+            Array.Sort(stringArray);
+
+            Console.WriteLine("The following sentance is the second by length: " + stringArray[stringArray.Length - 2]);
+
+            Console.ReadLine();
+
+        }
+
+
+        public static void message(string stringNumber)
+
+        {
+            Console.WriteLine("Enter your {0} sentance", stringNumber);
+        }
+
+
+    }
+}
